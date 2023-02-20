400120e18fda19396ca305afc7a74810dd2c70c3
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2023-02-21 00:14:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+var cors = require('cors')
+app.use(cors())
 connectTOMongo()
 
 dotenv.config();
